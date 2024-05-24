8da94c917f21204ccbda70bb314fb78633d6a32e
Stella-A <abbasovt_93@mail.ru>
2024-05-24 13:05:46 +0500

    1. Обновила стили таймера чтобы не было скачков при отсчете 2. Добавила проверку для избежания ненужной перерисовки DOM

--- source/js/modules/timer.js
+++ source/js/modules/timer.js
@@ -64,7 +64,15 @@ export default class Timer {
   }
 
   updateTime({minutes, seconds}) {
-    this.timerMinutesElement.textContent = Number(minutes) > 9 ? minutes : `0${minutes}`;
-    this.timerSecondsElement.textContent = Number(seconds) > 9 ? seconds : `0${seconds}`;
+    const formattedMinutes = Number(minutes) > 9 ? `${minutes}` : `0${minutes}`;
+    const formattedSeconds = Number(seconds) > 9 ? `${seconds}` : `0${seconds}`;
+
+    if (this.timerMinutesElement.textContent !== formattedMinutes) {
+      this.timerMinutesElement.textContent = formattedMinutes;
+    }
+
+    if (this.timerSecondsElement.textContent !== formattedSeconds) {
+      this.timerSecondsElement.textContent = formattedSeconds;
+    }
   }
 }
